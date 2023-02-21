a5b9ed25db569b4f6a2ca8cd736cf8f9909f222b
Omar Fahmy <Ofahmy1234@gmail.com>
2023-02-21 19:23:53 +0200

    Feat: Initialize react app with vite and yarn

---
+++ form/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Title from './components/creation-page/form-body/title/title.component'
+import FormBody from './components/creation-page/form-body/form-body.component'
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <FormBody />
+
+    </div>
+  )
+}
+
+export default App
