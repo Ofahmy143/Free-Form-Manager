3ccc6d473c0102e311e45d6d669f19dee939e91e
Omar Fahmy <Ofahmy1234@gmail.com>
2023-02-22 05:20:44 +0200

    Fix: Add Questions button in new form state

--- form/src/components/creation-page/form-body/form-body.component.tsx
+++ form/src/components/creation-page/form-body/form-body.component.tsx
@@ -3,26 +3,41 @@ import reactLogo from './assets/react.svg'
 import './form-body.component.css'
 import Question from './question/question.component'
 import Title from './title/title.component'
-import {FormQuestion} from '../../../types/form-body'
+import {Form, FormQuestion} from '../../../types/form-body'
 import { saveAs } from 'file-saver';
 
 
+
 function FormBody() {
-  const [questions , setQuestions] = useState<FormQuestion[]>([{
-    key:-1,
-    title:'Noname',
-    type:'Essay',
-    input: ['This is a place holder for essay' ,'This is the second placeholder']
-  }])
-  const [title, setTitle] = useState({
-    title: 'Untitled',
-    description: 'empty for now'
+  // const [questions , setQuestions] = useState<FormQuestion[]>([{
+  //   key:-1,
+  //   title:'Noname',
+  //   type:'Essay',
+  //   input: ['This is a place holder for essay' ,'This is the second placeholder']
+  // }])
+  // const [title, setTitle] = useState({
+  //   title: 'Untitled',
+  //   description: 'empty for now'
+  // })
+  const [form, setForm] = useState<Form>({
+    title: {
+      title: 'Untitled',
+      description: 'empty for now'
+    },
+    questions : [
+        {
+          key:0,
+          title:'Noname',
+          type:'Essay',
+          input: ['This is a place holder for essay' ,'This is the second placeholder']
+        }
+    ]
   })
 
-
   useEffect(()=>{
-    console.log(questions)
-  },[questions])
+    console.log(form)
+  },[form])
+
   function handleExportForm(){
     console.log(document.documentElement.innerHTML)
     let formHTML = document.documentElement.innerHTML;
@@ -34,31 +49,33 @@ function FormBody() {
     link.click()
 
   }
+
   function handleSaveForm(){
-    let form = {
-      title : title,
-      body: questions
-    }
+
+
     let formJson = JSON.stringify(form,null,4)
     console.log(formJson)
     saveAs(new Blob([formJson] , {type:'application/json'}) , 'form.json')
   }
+
   function handleAddQuestion(){
-    setQuestions([...questions,{key: questions.length,title:"Untitled",type:"MCQ",input:['This is first Placeholder']}])
+    let questions = form.questions
+    setForm({...form , questions: [...questions , {key: questions.length,title:"Untitled",type:"MCQ",input:['This is first Placeholder']}]})
+
   }
   return (
     <div className="form-body">
       <Title 
-      title={title.title}
-      description={title.description}
+      title={form.title.title}
+      description={form.title.description}
       />
-      {questions.map((question: FormQuestion,index: number)=>(
+      {form.questions.map((question: FormQuestion,index: number)=>(
         <Question 
         key={index}
         id={index}
         question = {question}
-        questionsArr = {questions}
-        OnQuestionChange = {setQuestions}
+        questionsArr = {form.questions}
+        OnQuestionChange = {setForm}
         />
       ))}
       <button onClick={handleSaveForm} >Save JSON</button>
