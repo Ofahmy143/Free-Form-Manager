dd996a7088836af878167dba3f3f88528e2c9cb3
Omar Fahmy <Ofahmy1234@gmail.com>
2023-02-23 05:46:46 +0200

    feat: Create header for the page

---
+++ form/src/components/Header/header.component.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './header.component.css'
+
+
+function Header() {
+  return (
+    <div className="Header">
+        <p>
+            Free 
+            Form Manager
+        </p>
+
+    </div>
+  )
+}
+
+export default Header
--- form/src/components/creation-page/form-body/title/title.component.tsx
+++ form/src/components/creation-page/form-body/title/title.component.tsx
@@ -1,16 +1,41 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './title.component.css'
-import {FormTitle} from '../../../../types/form-body'
+import { Form, FormTitle} from '../../../../types/form-body'
+
+type props = {
+  form: Form,
+  OnQuestionChange: React.Dispatch<React.SetStateAction<Form>>
+}
 
 
+function Title({ form, OnQuestionChange}: props) {
 
-function Title({title,img, description}: FormTitle) {
+  function handleFormTitleChange(event:React.ChangeEvent<HTMLInputElement>){
+    event.preventDefault();
+    let newTitle = event.target.value
+    
+
+    OnQuestionChange({...form, title: {...form.title, title: newTitle}})
+
+  }
+
+  function handleFormDescriptionChange(event:React.ChangeEvent<HTMLInputElement>){
+    event.preventDefault();
+    let newDescription = event.target.value
+
+    OnQuestionChange({...form, title: {...form.title, description: newDescription}})
+
+  }
 
   return (
     <div className="Title">
-        <h1>{title}</h1>
-        <p>{description}</p>
+      <h1>
+        <input value={form.title.title} onChange={handleFormTitleChange}/>
+      </h1>
+      <p>
+      <input value={form.title.description} onChange={handleFormDescriptionChange}/>
+      </p>
     </div>
   )
 }
