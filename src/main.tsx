b5e0f5557daaf536373d992c233af7ede6d7e6e8
Omar Fahmy <Ofahmy1234@gmail.com>
2023-03-08 01:48:08 +0200

    Feat: Import Bootstrap successfully

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,13 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App";
 import "./index.css";
+import { Provider } from "react-redux";
+import {store} from './redux/store'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
